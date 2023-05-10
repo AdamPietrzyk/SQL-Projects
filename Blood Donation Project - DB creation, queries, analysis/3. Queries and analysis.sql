9714e235e6917a94f9cda70f9f519015535ce008
AdamPietrzyk <127242593+AdamPietrzyk@users.noreply.github.com>
2023-05-10 18:35:26 +0200

    Update 3. Queries and analysis.sql

--- Blood Donation Project - DB creation, queries, analysis/3. Queries and analysis.sql
+++ Blood Donation Project - DB creation, queries, analysis/3. Queries and analysis.sql
@@ -1,29 +1,36 @@
-# 1. Ile krwi oddały kobiety, a ile mężczyźni? Łącznie, bez znaczenia grupa i czynnik Rh.
+# 1. How much blood did women and men donate? In total, regardless of blood type and Rh factor.
 SELECT p.gender, SUM(d.DonationQuantity)
 FROM Patients p
 	JOIN Donations d
 		ON p.ID_Patient = d.ID_Patient
 GROUP BY p.gender;
 
-# 2. Ilu dawców to kobiety, a ilu dawców to mężczyźni?
+
+
+#2. How many donors are women, and how many donors are men?
 SELECT COUNT(ID_Patient), gender
 FROM Patients
 GROUP BY gender;
 
-# 3. Ilu dawców ma na nazwisko "Kowalski"?
+
+
+#3. How many donors have the last name "Kowalski"?
 SELECT COUNT(*), last_name
 FROM Patients
 WHERE last_name LIKE "Kowalski"
 GROUP BY last_name;
 
-# 4. Ilu dawców ma jaką grupę krwi?
+
+#4. How many donors have which blood type?
 SELECT COUNT(*), bt.BloodType
 FROM Patients p
 	JOIN BloodType bt
 		ON p.ID_BloodType = bt.ID_BloodType
 GROUP BY bt.BloodType;
 
-#5. Ilu dawców ma jaką grupę krwi oraz czynnik Rh?
+
+
+#5. How many donors have each combination of blood type and Rh factor?
 SELECT bt.BloodType, rf.RhFactor, COUNT(*)
 FROM Patients p
 	JOIN BloodType bt
@@ -33,12 +40,14 @@ FROM Patients p
 GROUP BY bt.BloodType, rf.RhFactor
 ORDER BY bt.BloodType;
 
-#5. Jaki jest udział procentowy osób z daną grupą krwi oraz czynnikiem Rh w ogólnej liczbie osób?
+
+
+#6. What is the percentage of individuals with each blood type and Rh factor in the total number of individuals?
 SELECT
 	bt.BloodType,
-    rf.RhFactor,
-    COUNT(*),
-    ROUND(count(*) * 100.0 / sum(count(*)) Over(), 2) AS Percentage
+    	rf.RhFactor,
+    	COUNT(*),
+    	ROUND(count(*) * 100.0 / sum(count(*)) Over(), 2) AS Percentage
 FROM Patients p
 	JOIN BloodType bt
 		ON p.ID_BloodType = bt.ID_BloodType
@@ -47,12 +56,14 @@ FROM Patients p
 GROUP BY bt.BloodType, rf.RhFactor
 ORDER BY BloodType;
 
-#6. Jaki jest udział procentowy ilości danej grupy krwi oraz danego czynnika Rh w ogólnej liczbie krwi?
+
+
+#7. What is the percentage of each combination of blood type and Rh factor in the overall blood volume?
 SELECT
 	bt.BloodType,
-    rf.RhFactor,
-    SUM(d.DonationQuantity) AS 'Sum of donated quantity',
-    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (),2) AS Percentage
+   	rf.RhFactor,
+   	SUM(d.DonationQuantity) AS 'Sum of donated quantity',
+    	ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (),2) AS Percentage
 FROM Patients p
 	JOIN Donations d
 		ON p.ID_Patient = d.ID_Patient
@@ -63,13 +74,17 @@ FROM Patients p
 GROUP BY bt.BloodType, rf.RhFactor
 ORDER BY BloodType;
 
-#7. Ile osób jest z którego miasta?
+
+
+#8. How many individuals are from each city?
 SELECT COUNT(*), address_city
 FROM Patients
 GROUP BY address_city
 ORDER BY COUNT(*) DESC;
 
-#8. Ile pobrań w której placówce i jaka to grupa krwi oraz jaki czynnik Rh oraz jaka płeć?
+
+
+#9. How many donations took place at each institution, and what is the blood type, Rh factor, and gender associated with each donation?
 SELECT
 	COUNT(*),
     bt.BloodType,
@@ -88,7 +103,8 @@ FROM Patients p
 GROUP BY bt.BloodType, rf.RhFactor, i.ID_Institution, p.gender
 ORDER BY i.ID_Institution, bt.BloodType, rf.RhFactor;
 
-#9. W którym miesiącu ile krwi i jaka grupa?
+
+#10. How much blood (blood type and Rh factor) was donated in each month?
 SELECT
 	MONTH(d.Donation_Date) as Months,
     bt.BloodType,
@@ -104,7 +120,9 @@ FROM Patients p
 	JOIN BloodType bt
 		ON p.ID_BloodType = bt.ID_BloodType
 GROUP BY Months, bt.BloodType, rf.RhFactor WITH ROLLUP;
-# z if grouping
+
+# Same as above, but using "if grouping"
+
 SELECT
 	MONTH(d.Donation_Date) as Months,
 		IF(GROUPING(bt.BloodType),
@@ -122,19 +140,25 @@ FROM Patients p
 		ON p.ID_BloodType = bt.ID_BloodType
 GROUP BY Months, bt.BloodType, rf.RhFactor WITH ROLLUP;
 
-# 10. Ile łącznie krwi oddał pacjent o ID 111?
+
+
+#11. How much blood did the patient with ID 111 donate in total?
 SELECT CONCAT(p.first_name, ' ', p.last_name), SUM(d.DonationQuantity)
 FROM Patients p
 	JOIN Donations d
 		ON p.ID_Patient = d.ID_Patient
 WHERE d.ID_Patient = 111;
 
-# 11. Sprawdzenie czy jest pacjent, który nie oddał krwi w ogóle?
+
+
+#12. Checking if there is a patient who has not donated any blood at all?
 SELECT * FROM Patients
 WHERE DonatedAmount IS NULL;
 #te 2 osoby nie oddały krwi w ogóle
 
-# 12. Przygotuj dowolne zapytanie wykorzystując case when.
+
+
+#13. Prepare a query using CASE WHEN statement.
 SELECT
 	ID_Patient, first_name, last_name,
 CASE
@@ -143,7 +167,9 @@ CASE
 END
 FROM Patients;
 
-# 13. Napisz zapytanie z podzapytaniem
+
+
+#14. Write a query with a subquery.
 SELECT
 	ID_Patient,
     first_name,
@@ -152,9 +178,10 @@ SELECT
 FROM Patients
 WHERE DonatedAmount > (SELECT AVG(DonatedAmount) FROM Patients);
 
-# 14. Wykorzystaj derived table
-# Jednym z przykładów zapytania z wykorzystaniem derived table może być zapytanie wybierające sumę
-# wszystkich donacji z Inventory dla każdej grupy krwi:
+
+
+# 15. Use derived table
+# An example of a query using a derived table can be a query selecting the sum of all donations from Inventory for each blood type.
 SELECT bt.BloodType, rf.RhFactor, sum(inv.Quantity) AS TotalQuantity
 FROM BloodType bt
 CROSS JOIN RhFactor rf
@@ -165,9 +192,10 @@ LEFT JOIN (
 GROUP BY bt.BloodType, rf.RhFactor
 ORDER BY bt.BloodType, rf.RhFactor;
 
-# 15. Wykorzystaj CTE
-# Przykładowe zapytanie z wykorzystaniem Common Table Expression (CTE),
-# które znajduje najnowszą datę pobrania krwi dla każdego typu krwi i RhFactor w tabeli Donations:
+
+
+# 16. Use CTE
+# Example query using Common Table Expression (CTE) that finds the latest blood donation date for each blood type and RhFactor in the Donations table.
 WITH LatestDonations AS (
 	SELECT p.ID_BloodType, p.ID_RhFactor, MAX(d.Donation_Date) AS LatestDate
     FROM Donations d
